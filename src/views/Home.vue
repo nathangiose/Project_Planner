8e0201306d58f197432a94692a68d8724926d5a8
nathangiose <nathangiose@gmail.com>
2021-11-14 03:11:16 +0200

    Completed the filters: Project-Planner Complete

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,28 +2,31 @@
   <div class="home">
     <FilterNav :current="current" @filterChange="current = $event" />
     <div v-if="projects.length">
-      <div v-for="project in projects" :key="project.id">
-        <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
+      <div v-for="project in filteredProjects" :key="project.id">
+      <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+// challenge
+//   - when the filter changes, only show those projects
+//   - e.g. if we click 'completed' only show completed project
+//   - use a computed property to do this
 import SingleProject from '../components/SingleProject.vue'
 import FilterNav from '../components/FilterNav.vue'
-
 export default {
   name: 'Home',
   components: { SingleProject, FilterNav },
   data() {
     return {
       projects: [],
-      current: 'all'
+      current: 'all',
     };
   },
   mounted() {
-    fetch('http://localhost:3000/projects/')
+    fetch('http://localhost:3000/projects')
       .then(res => res.json())
       .then(data => this.projects = data)
       .catch(err => console.log(err))
@@ -39,8 +42,18 @@ export default {
         return project.id === id
       })
       p.complete = !p.complete 
-      // console.log(p)
     }
-  }
+  },
+  computed: {
+    filteredProjects() {
+      if (this.current === 'completed') {
+        return this.projects.filter(project => project.complete)
+      }
+      if (this.current === 'ongoing') {
+        return this.projects.filter(project => !project.complete)
+      }
+      return this.projects
+    }
+  },
 }
 </script>
